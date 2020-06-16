08cc6ed661c08c33a0c3f5a5e8a6b401bb9d2ac8
Roberto Santini <roberto.santini89@gmail.com>
2020-06-16 11:32:38 +0200

    .gitignore fix

--- Modules/RegistroPresenze/Config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'name' => 'Registro Presenze',
-    'permissions' => array(
-      'view-certificazione' => 'Visualizza i registri presenze',
-      'edit-certificazione' => 'Modifica i registri presenze',
-    )
-];
